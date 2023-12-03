fb108b3425a972223864bce7afcd6cb1befce505
JSChen4 <jchen3014@gatech.edu>
2023-12-03 16:36:18 -0500

    changed sleep data to profit data

--- CozyEats/Seller/SellerHomeView.swift
+++ CozyEats/Seller/SellerHomeView.swift
@@ -1,43 +1,22 @@
 import SwiftUI
 import Charts
 
-struct SleepDataPoint: Identifiable {
+struct ProfitDataPoint: Identifiable {
     var id = UUID().uuidString
     var day: String
     var profits: Int
 }
 
 struct SellerHomeView: View {
-    
     var data = [
-        SleepDataPoint(
-        day: "Mon",
-        profits: 5),
-        
-        SleepDataPoint(
-        day: "Tue",
-        profits: 3),
-        
-        SleepDataPoint(
-        day: "Wed",
-        profits: 10),
-        
-        SleepDataPoint(
-        day: "Thu",
-        profits: 2),
-        
-        SleepDataPoint(
-        day: "Fri",
-        profits: 6),
-        
-        SleepDataPoint(
-        day: "Sat",
-        profits: 3),
-        
-        SleepDataPoint(
-        day: "Sun",
-        profits: 7)]
-    
+        ProfitDataPoint(day: "Mon", profits: 5),
+        ProfitDataPoint(day: "Tue", profits: 3),
+        ProfitDataPoint(day: "Wed", profits: 10),
+        ProfitDataPoint(day: "Thu", profits: 2),
+        ProfitDataPoint(day: "Fri", profits: 6),
+        ProfitDataPoint(day: "Sat", profits: 3),
+        ProfitDataPoint(day: "Sun", profits: 7)
+    ]
     
     var body: some View {
         NavigationView {
@@ -45,16 +24,16 @@ struct SellerHomeView: View {
                 Color.tan.edgesIgnoringSafeArea(.all)
                 ScrollView {
                     Chart {
-                        ForEach (data) { d in
-                            BarMark( //change BareMark to LineMark to get line graph
+                        ForEach(data) { d in
+                            BarMark(
                                 x: .value("Day", d.day),
                                 y: .value("Profits", d.profits))
-                            .annotation{
-                                Text(String(d.profits))
-                            }
+                                .annotation{
+                                    Text(String(d.profits))
+                                }
                         }
                     }
-                    // Your existing content
+                    
                     VStack(alignment: .leading, spacing: 10) {
                         Text("Today's Profits:")
                             .font(.headline)
@@ -65,10 +44,10 @@ struct SellerHomeView: View {
                         Text("Your 30-Day Profits:")
                             .font(.headline)
                             .padding(.top)
-
+                        
                         Text("Page Views (last 30 days):")
                             .font(.headline)
-
+                        
                         Text("Best Seller:")
                             .font(.headline)
                         
@@ -81,12 +60,12 @@ struct SellerHomeView: View {
             .navigationTitle("Your Business Data")
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    // Add any additional toolbar items if needed
                 }
             }
         }
     }
 }
+
 
 // Preview
 struct SellerHomeView_Previews: PreviewProvider {
