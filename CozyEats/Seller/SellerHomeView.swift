8333f0c292c72f795e3c4f2bd9c4571d7e72029b
JSChen4 <jchen3014@gatech.edu>
2023-12-03 16:15:26 -0500

    added ideas on what data to display

--- CozyEats/Seller/SellerHomeView.swift
+++ CozyEats/Seller/SellerHomeView.swift
@@ -33,26 +33,39 @@ struct SellerHomeView: View {
                 Color.tan.edgesIgnoringSafeArea(.all)
                 ScrollView {
                     // Your existing content
-                    Text("Your existing content here")
-                        .padding()
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Today's Profits:")
+                            .font(.headline)
+                        
+                        Text("Your 7-Day Profits:")
+                            .font(.headline)
+                        
+                        Text("Your 30-Day Profits:")
+                            .font(.headline)
+                            .padding(.top)
 
-                    // Display an empty bar graph
-                    BarGraphView(data: emptyData, barColor: .blue) // Customize color as needed
-                        .frame(height: 200) // Adjust height as per requirement
-                        .padding()
+                        Text("Page Views (last 30 days):")
+                            .font(.headline)
+
+                        Text("Best Seller:")
+                            .font(.headline)
+                        
+                        Text("Total Orders:")
+                            .font(.headline)
+                        
+
+                        // Display an empty bar graph
+                        BarGraphView(data: emptyData, barColor: .blue) // Customize color as needed
+                            .frame(height: 200) // Adjust height as per requirement
+                            .padding()
+                    }
+                    .padding()
                 }
             }
-            .navigationTitle("Cozy Eats")
+            .navigationTitle("Your Business Data")
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink {
-                        Text("Add")
-                    } label: {
-                        Image(systemName: "plus")
-                            .foregroundColor(.primary)
-                            .font(.headline)
-                            .fontWeight(.semibold)
-                    }
+                    // Add any additional toolbar items if needed
                 }
             }
         }
