859538ed8425021ab890a1fd4ce5fb17d6fa3525
Benjamin <bmelville8@gatech.edu>
2023-12-04 09:20:09 -0500

    final updates graphs

---
+++ CozyEats/Seller/CustomerFavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomerFavoritesView.swift
+//  CozyEats
+//
+//  Created by Benjamin Melville on 12/4/23.
+//
+
+import SwiftUI
+
+struct CustomerFavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomerFavoritesView()
+}
